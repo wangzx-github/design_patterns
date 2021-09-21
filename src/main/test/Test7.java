9d0e5e682b4b12e1d7cfe4c4223f45020d658be4
wangzx <mrwangzx@aliyun.com>
2021-09-22 01:12:04 +0800

    dierci

---
+++ src/main/test/Test7.java
@@ -0,0 +1,11 @@
+package main.test;
+
+/**
+ * @author wangzx
+ * @create 2021-09-1:11
+ */
+public class Test7 {
+    public static void main(String[] args) {
+        System.out.println("dfjsdklfjalsdfjl;aksdjkf;las");
+    }
+}
